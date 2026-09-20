f28523118e56737f4c01efb49303b83404ff5658
Melvin A. Lumanta Jr. <lumantamelvin69@gmail.com>
2026-09-21 00:52:03 +0800

    4th commit

---
+++ Grade checker.js
@@ -0,0 +1,11 @@
+let grade = 88;
+
+if (grade >= 90) {
+    console.log("Excellent");
+} else if (grade >= 80) {
+    console.log("Very Good");
+} else if (grade >= 75) {
+    console.log("Passed");
+} else {
+    console.log("Failed");
+}
