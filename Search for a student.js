35f1b781961591d9f10dc9481ac175e64ea88624
Melvin A. Lumanta Jr. <lumantamelvin69@gmail.com>
2026-09-21 00:55:30 +0800

    7th commit

---
+++ Search for a student.js
@@ -0,0 +1,9 @@
+let students = ["Juan", "Mark", "Anna", "Carlos"];
+
+let searchName = "Anna";
+
+if (students.includes(searchName)) {
+    console.log(searchName + " was found.");
+} else {
+    console.log(searchName + " was not found.");
+}
