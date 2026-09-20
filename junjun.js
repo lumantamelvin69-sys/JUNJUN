2e2f6c477287be58c033280075dc093ea57e292c
Melvin A. Lumanta Jr. <lumantamelvin69@gmail.com>
2026-09-21 00:30:17 +0800

    first commit

---
+++ junjun.js
@@ -0,0 +1,16 @@
+let name = "Melvin";
+let age = 20;
+let course = "BS Information Technology";
+
+console.log("Student Information");
+console.log("-------------------");
+console.log("Name: " + name);
+console.log("Age: " + age);
+console.log("Course: " + course);
+
+// Check if the student is an adult
+if (age >= 18) {
+    console.log("Status: Adult");
+} else {
+    console.log("Status: Minor");
+}
