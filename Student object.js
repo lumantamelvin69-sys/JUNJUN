56d13141ba1cdfb300ba5e83c41c8f3675946ae8
Melvin A. Lumanta Jr. <lumantamelvin69@gmail.com>
2026-09-21 00:56:46 +0800

    8th commit

---
+++ Student object.js
@@ -0,0 +1,11 @@
+let student = {
+    name: "Juan",
+    age: 20,
+    course: "BSIT",
+    grade: 88
+};
+
+console.log("Name:", student.name);
+console.log("Age:", student.age);
+console.log("Course:", student.course);
+console.log("Grade:", student.grade);
