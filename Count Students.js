b286010a78f7851efacf6c770226f3d0ebe63daf
Melvin A. Lumanta Jr. <lumantamelvin69@gmail.com>
2026-09-21 00:57:58 +0800

    9th commit

---
+++ Count Students.js
@@ -0,0 +1,22 @@
+let students = [
+    {
+        name: "Juan",
+        age: 20,
+        course: "BSIT",
+        grade: 88
+    },
+    {
+        name: "Mark",
+        age: 21,
+        course: "BSIT",
+        grade: 92
+    },
+    {
+        name: "Anna",
+        age: 19,
+        course: "BSIT",
+        grade: 85
+    }
+];
+
+console.log("Total Students:", students.length);
